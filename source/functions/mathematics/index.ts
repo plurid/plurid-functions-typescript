0a5a5338e3f35bb131dec89497eaa1493893c725
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-15 12:12:04 +0200

    dev: mathematics

---
+++ source/functions/mathematics/index.ts
@@ -0,0 +1,11 @@
+import * as arithmetic from './arithmetic';
+import * as geometry from './geometry';
+import * as random from './random';
+
+
+
+export {
+    arithmetic,
+    geometry,
+    random,
+}
