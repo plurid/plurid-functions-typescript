2e738df1e191270b7e9f7f30e0246e3b5064d41d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-01-14 19:46:36 +0200

    dev: functions

--- source/functions/uuid/index.ts
+++ source/functions/uuid/index.ts
@@ -5,7 +5,9 @@
  *
  * @param separator
  */
-export const uuidv4 = (separator: string = '') => {
+export const uuidv4 = (
+    separator: string = '',
+) => {
     return (
         [1e7] as any + separator + 1e3 + separator + 4e3 + separator + 8e3 + separator + 1e11).replace(/[018]/g,
         (c: any) => (((c ^ crypto.getRandomValues(new Uint8Array(1))[0]) & 15) >> c / 4).toString(16)
