2f94a6ef2e157ad9ae39dc29cb6cdd7d47a3bc3c
Jan Cavel <caveljan@gmail.com>
2020-06-23 15:11:56 +0300

    plurid-functions-general > dev: objects

--- packages/plurid-functions-general/source/functions/objects/index.ts
+++ packages/plurid-functions-general/source/functions/objects/index.ts
@@ -1,12 +1,12 @@
 /**
  * http://blog.nicohaemhouts.com/2015/08/03/accessing-nested-javascript-objects-with-string-key/
  *
- * @param theObject
+ * @param data
  * @param path
  * @param separator
  */
-export const getNested = (
-    theObject: any,
+export const getNested = <T>(
+    data: T,
     path: string,
     separator: string = '.',
 ) => {
@@ -18,9 +18,59 @@ export const getNested = (
                 (obj: any, property: string) => {
                     return obj[property];
                 },
-                theObject,
+                data,
             );
     } catch (err) {
         return undefined;
+    }
+}
+
+
+/**
+ * https://stackoverflow.com/a/52912610
+ *
+ * @param data
+ * @param update
+ * @param path
+ * @param separator
+*/
+export const updateNested = <T, U>(
+   data: T,
+   path: string,
+   update: U,
+   separator: string = '.',
+): T | undefined => {
+    try {
+        const split = path.split(separator);
+
+        let current = data;
+
+        while (split.length > 1) {
+            const part = split.shift();
+
+            if (!part) {
+                return;
+            }
+
+            const parent = current;
+            current = (current as any)[part];
+
+            if (undefined === current) {
+                (parent as any)[part] = {};
+                current = (parent as any)[part];
+            }
+        }
+
+        if (split.length === 1) {
+            const part = split.shift();
+            if (!part) {
+                return;
+            }
+            (current as any)[part] = update;
+        }
+
+        return data;
+    } catch (err) {
+        return;
     }
 }
