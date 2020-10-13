659d40b10bac82b9ac704087f0ab8ee25e2965f2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-13 16:39:24 +0300

    plurid-functions-general > dev: objects

--- packages/plurid-functions-general/source/functions/objects/index.ts
+++ packages/plurid-functions-general/source/functions/objects/index.ts
@@ -74,3 +74,71 @@ export const updateNested = <T, U>(
         return;
     }
 }
+
+
+/**
+ * https://stackoverflow.com/a/40294058/6639124
+ *
+ * @param obj
+ * @param hash
+ */
+const deepClone = (
+    obj: any,
+    hash = new WeakMap(),
+): any => {
+    if (Object(obj) !== obj) return obj; // primitives
+
+    if (hash.has(obj)) return hash.get(obj); // cyclic reference
+
+    const result = obj instanceof Set ? new Set(obj) // See note about this!
+                 : obj instanceof Map ? new Map(Array.from(obj, ([key, val]) =>
+                                        [key, deepClone(val, hash)]))
+                 : obj instanceof Date ? new Date(obj)
+                 : obj instanceof RegExp ? new RegExp(obj.source, obj.flags)
+                 // ... add here any specific treatment for other classes ...
+                 // and finally a catch-all:
+                 : obj.constructor ? new obj.constructor()
+                 : Object.create(null);
+
+    hash.set(obj, result);
+
+    return Object.assign(
+        result,
+        ...Object.keys(obj).map(
+            key => ({ [key]: deepClone(obj[key], hash) })
+        ),
+    );
+}
+
+
+/**
+ * Creates a deep clone of the `data`.
+ *
+ * The default `type` is `json`, meant for deep cloning of json-like objects.
+ * The `any` `type` will handle a deep clone of `Function`, `Date`, and more.
+ *
+ * @param data
+ * @param type
+ */
+export const clone = <D>(
+    data: D | undefined,
+    type?: 'json' | 'any',
+) => {
+    if (!data) {
+        return;
+    }
+
+    if (!type || type === 'json') {
+        try {
+            const cloned = JSON.parse(JSON.stringify(data));
+
+            return cloned;
+        } catch (error) {
+            return;
+        }
+    }
+
+    const deepCloned = deepClone(data);
+
+    return deepCloned;
+}
