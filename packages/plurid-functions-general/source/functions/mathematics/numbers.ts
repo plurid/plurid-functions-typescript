b2bbae2321213e0bb784e46e0336e5da52a42d3d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-25 10:04:20 +0300

    plurid-functions-general > dev: functions/mathematics/numbers

--- packages/plurid-functions-general/source/functions/mathematics/numbers.ts
+++ packages/plurid-functions-general/source/functions/mathematics/numbers.ts
@@ -17,4 +17,35 @@ export const checkIntegerNonUnit = (
 
     return false;
 }
+
+
+/**
+ * Normalizes the `value` between the `lower` and `upper` limits.
+ *
+ * ```
+ * normalizeBetween( 20, 0, 100) -> 20
+ * normalizeBetween(-20, 0, 100) -> 0
+ * normalizeBetween(120, 0, 100) -> 100
+ * ```
+ *
+ * @param value
+ * @param lower
+ * @param upper
+ * @returns
+ */
+ export const normalizeBetween = (
+    value: number,
+    lower: number,
+    upper: number,
+) => {
+    if (value < lower) {
+        return lower;
+    }
+
+    if (value > upper) {
+        return upper;
+    }
+
+    return value;
+}
 // #endregion module
