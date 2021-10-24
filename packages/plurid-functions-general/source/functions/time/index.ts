ba6b21497550ff8543fbfda8cc203fb6afd8b319
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-10-24 17:43:57 +0300

    plurid-functions-general > dev: time | delay

--- packages/plurid-functions-general/source/functions/time/index.ts
+++ packages/plurid-functions-general/source/functions/time/index.ts
@@ -11,6 +11,24 @@ const stamp = () => {
 
     return `${date.toLocaleTimeString()} - ${date.toLocaleDateString()}`;
 }
+
+
+/**
+ * Delay the program flow for `value` time, in milliseconds.
+ *
+ * Default: `500`
+ *
+ * @param value
+ */
+const delay = async (
+    value: number = 500,
+) => {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, value);
+    });
+}
 // #endregion module
 
 
@@ -19,5 +37,6 @@ const stamp = () => {
 export {
     now,
     stamp,
+    delay,
 };
 // #endregion exports
