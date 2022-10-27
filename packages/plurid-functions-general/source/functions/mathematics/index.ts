a03954bef6874ffd5968bc7fdcb18ac745ff1319
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-10-27 16:54:03 +0300

    plurid-functions-general > dev: mathematics/constants | tau

--- packages/plurid-functions-general/source/functions/mathematics/index.ts
+++ packages/plurid-functions-general/source/functions/mathematics/index.ts
@@ -1,6 +1,7 @@
 // #region imports
     // #region internal
     import * as arithmetic from './arithmetic';
+    import * as constants from './constants';
     import * as geometry from './geometry';
     import * as numbers from './numbers';
     import * as random from './random';
@@ -12,6 +13,7 @@
 // #region exports
 export {
     arithmetic,
+    constants,
     geometry,
     numbers,
     random,
